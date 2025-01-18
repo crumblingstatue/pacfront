404de6583a376ce4093776d08d15f20e1db9393c
crumblingstatue <radiantstatue@gmail.com>
2025-01-18 10:34:00 +0100

    Extract tab viewer code into app::ui::tabs module

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -2,16 +2,12 @@ use {
     super::{PacfrontApp, ouroboros_impl_pac_state::PacState},
     cmd::CmdBuf,
     eframe::egui,
-    egui_dock::{DockArea, DockState, TabViewer},
-    tabs::package::PkgTab,
+    egui_dock::{DockArea, DockState},
+    tabs::{Tab, TabViewState},
 };
 
-mod tabs {
-    pub mod local_pkg_list;
-    pub mod package;
-    pub mod remote_pkg_list;
-}
 pub mod cmd;
+mod tabs;
 
 pub(super) struct UiState {
     dock_state: DockState<Tab>,
@@ -31,67 +27,6 @@ impl Default for UiState {
             dock_state: DockState::new(vec![Tab::LocalDb, Tab::SyncDbPkgList]),
         }
     }
-}
-
-struct TabViewState<'pac, 'ui> {
-    pac: &'pac mut PacState,
-    ui: &'ui mut SharedUiState,
-}
-
-impl TabViewer for TabViewState<'_, '_> {
-    type Tab = Tab;
-
-    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
-        match tab {
-            Tab::LocalDb => format!("Local packages ({})", self.pac.borrow_pkg_list().len()).into(),
-            Tab::SyncDbPkgList => format!(
-                "Remote packages ({})",
-                self.pac
-                    .borrow_sync()
-                    .iter()
-                    .map(|db| db.pkgs().len())
-                    .sum::<usize>()
-            )
-            .into(),
-            Tab::LocalPkg(pkg) => format!("Package '{}'", pkg.name).into(),
-            Tab::RemotePkg(pkg) => format!("Remote Package '{}'", pkg.name).into(),
-        }
-    }
-
-    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        match tab {
-            Tab::LocalDb => tabs::local_pkg_list::ui(ui, self.pac, self.ui),
-            Tab::SyncDbPkgList => tabs::remote_pkg_list::ui(ui, self.pac, self.ui),
-            Tab::LocalPkg(tab) => tabs::package::ui(ui, self.pac, self.ui, tab, false),
-            Tab::RemotePkg(tab) => tabs::package::ui(ui, self.pac, self.ui, tab, true),
-        }
-    }
-
-    fn closeable(&mut self, tab: &mut Self::Tab) -> bool {
-        #[expect(clippy::match_like_matches_macro)]
-        match tab {
-            Tab::LocalDb => false,
-            _ => true,
-        }
-    }
-
-    fn force_close(&mut self, tab: &mut Self::Tab) -> bool {
-        match tab {
-            Tab::LocalDb => false,
-            Tab::LocalPkg(pkg_tab) => pkg_tab.force_close,
-            Tab::RemotePkg(pkg_tab) => pkg_tab.force_close,
-            Tab::SyncDbPkgList => false,
-        }
-    }
-}
-
-#[derive(Default)]
-pub enum Tab {
-    #[default]
-    LocalDb,
-    SyncDbPkgList,
-    LocalPkg(PkgTab),
-    RemotePkg(PkgTab),
 }
 
 pub fn top_panel_ui(_app: &mut PacfrontApp, ctx: &egui::Context) {
